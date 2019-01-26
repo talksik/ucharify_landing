1bb0b81ebc2d18df0d560e6fc677c501a0b86cd4
Arjun Patel <patel.arjun50@gmail.com>
2019-01-26 14:15:08 -0800

    adding in proper images for slideshow, and main features section text and footer

--- dist/js/added.js
+++ dist/js/added.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // popup when they try to log in or signup
-  $('#login, #signup').click(function() {
+  $('#login, #signup, .social-media-icons').click(function() {
     $('#popup')
       .text('Coming Soon!')
       .slideDown()
@@ -47,4 +47,22 @@ $(document).ready(function() {
     `)
     );
   });
+
+  // Slideshow main
+  var myIndex = 0;
+  carousel();
+
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName('mySlides');
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = 'none';
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+      myIndex = 1;
+    }
+    x[myIndex - 1].style.display = 'block';
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+  }
 });
